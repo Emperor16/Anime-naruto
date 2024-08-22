88e26b46a9a1322e5e2b40b0650316d2308101b3
Emperor16 <olamided116@gmail.com>
2024-08-22 16:54:15 +0100

    test

---
+++ script.js
@@ -0,0 +1,11 @@
+let menu= document.querySelector("menu-icon");
+let navmenu = document.querySelector('.nav-menu');
+
+menu.onclick= () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle("open")
+};
+
+const sr= ScrollReveal({
+    display
+})
